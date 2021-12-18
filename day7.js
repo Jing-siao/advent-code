80ccbe5cddaaf0405f5e582556f4d93d3ba61f70
Jing-siao <vicky820715@gmail.com>
2021-12-18 14:43:48 +0800

    test

--- day7.js
+++ day7.js
@@ -2,14 +2,14 @@
 let text = document.getElementById('day7').innerHTML;
 let lines = text.split(",").map(Number);
 let plus = 0;
-for (let i = 0; i < lines.length; i++) {
-  let test = [];
-  for (let line of lines) {
-    test.push(Math.abs(line -= i))
-  };
-  plus = test.reduce((a, b) => a + b, 0);
+// for (let i = 0; i < lines.length; i++) {
+//   let test = [];
 
-}
+// }
+for (let line of lines) {
+  test.push(Math.abs(line -= i))
+};
+plus = test.reduce((a, b) => a + b, 0);
 
 
 console.log(plus)
