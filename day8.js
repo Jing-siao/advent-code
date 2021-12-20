97858b470ae39fca56cc3e0d5c06b393f04713b7
jing <vicky820715@gmail.com>
2021-12-20 17:30:14 +0800

    day8 part1 finish

--- day8.js
+++ day8.js
@@ -1,33 +1,94 @@
 // part 1
-let text = document.getElementById('day7').innerHTML;
-let lines = text.split(",").map(Number);
-let step1 = [];
-for (let i = 0; i < lines.length; i++) {
-  let temp = [];
-  let plus = 0;
-  for (let line of lines) {
-    temp.push(Math.abs(line -= i));
-  };
-  plus = temp.reduce((a, b) => a + b, 0);
-  step1.push(plus);
-  temp = [];
+let text = document.getElementById('day8').innerHTML;
+// n代表换行，\r代表回车，可以加个\s代表空格
+let AllArr = text.split(/[(\r\n)]+/);
+let splitArr = [];
+
+let partCount = 0;
+for (let i = 0; i < AllArr.length; i++) {
+  splitArr.push(AllArr[i].split(" | "));
+
 };
-console.log("part1:", Math.min(...step1))
-// part 2
-let step2 = 0
-let temp2 = [];
-let fuel = []
-for (let z = 1; z <= lines.length; z++) {
-  for (let line of lines) {
-    temp2.push(Math.abs(line -= z));
-  };
-  for (let i = 0; i < lines.length; i++) {
-    for (let y = 0; y < temp2[i] + 1; y++) {
-      step2 += y;
+let arr = [];
+let arr2 = [];
+//轉二維陣列
+for (let i = 0; i < AllArr.length; i++) {
+  let arr1 = [...[splitArr[i][0].split(" ")]];
+  let arr2 = [...[splitArr[i][1].split(" ")]];
+  arr.push([...arr1, ...arr2]);
+
+};
+let marked = [];
+for (let i = 0; i < 10; i++) {
+  let obj = {
+    num0: "",
+    num1: "",
+    num2: "",
+    num3: "",
+    num4: "",
+    num5: "",
+    num6: "",
+    num7: "",
+    num8: "",
+    num9: "",
+  }
+  marked.push(obj);
+};
+for (let i = 0; i < arr.length; i++) {
+  //抓出1,4,7,8
+  function forArr(arrLength, index) {
+    for (let y = 0; y < arrLength; y++) {
+      let length = arr[i][index][y].length;
+      let text = arr[i][index][y];
+      switch (length) {
+        case 2:
+          marked[i].num1 = Array.from(text);
+          break;
+        case 3:
+          marked[i].num7 = Array.from(text);
+          break;
+        case 4:
+          marked[i].num4 = Array.from(text);
+          break;
+        case 7:
+          marked[i].num8 = Array.from(text);
+          break;
+      };
     };
   };
-  fuel.push(step2)
-  step2 = 0
-  temp2 = [];
-}
-console.log("part1:", Math.min(...fuel))
+  //part1加總
+  if (length === 2 || length === 3 || length === 4 || length === 7) {
+    partCount += 1;
+  };
+  forArr(4, 1);
+  forArr(10, 0);
+};
+console.log('part1:', partCount)
+// part 2
+console.log(marked)
+for (let i = 0; i < AllArr.length; i++) {
+
+  for (let y = 0; y < 10; y++) {
+    let length = arr[i][0][y].length;
+    let text = arr[i][0][y];
+    if (length === 6) {
+      if (text.includes(marked[i].num4[0]) && text.includes(marked[i].num4[1]) && text.includes(marked[i].num4[2]) && text.includes(marked[i].num4[3])) {
+        console.log(text, true)
+      } else {
+        console.log(text, false)
+      }
+    }
+  };
+
+
+};
+
+
+console.log(arr)
+// let a = 'abcde';
+// let b = Array.from(a);
+// console.log(b);
+
+// const pets = ['cat', 'dog', 'bat'];
+
+// console.log(pets.includes('cat'));
